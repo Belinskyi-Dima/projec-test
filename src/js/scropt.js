6403b14e33f1ad00f86c88fe08ad3a58feced310
Belinskyi-Dima <84097875+Belinskyi-Dima@users.noreply.github.com>
2022-06-11 02:28:07 +0300

    delite console all

--- src/js/scropt.js
+++ src/js/scropt.js
@@ -1,27 +1,12 @@
 (() => {
-	// const menuBtnRef = document.querySelector("[data-menu-button]");
-	// const mobileMenuRef = document.querySelector("[data-menu]");
-	// const mobileBtnClose = document.querySelector("[data-menu-close]");
-   const hamburgerMenu = $('.mobile-menu')
-
-
-	$(document).on('click', '.js-hamburger-cta', (e)=>{
-		// alert("aaa")
-		// console.log(this);
-		console.log(hamburgerMenu);
-		e.preventDefault();
-		$(e.target).toggleClass('expanded');
-		hamburgerMenu.toggleClass('mobile-menu')
-	});
-
-	// menuBtnRef.addEventListener("click", () => {
-	//   mobileMenuRef.classList.toggle("is-open");
-	// });
- 
-	// mobileBtnClose.addEventListener("click", () => {
-	//   mobileMenuRef.classList.toggle("is-open");
-	// });
- })();
+  const hamburgerMenu = $(".mobile-menu");
+  $(document).on("click", ".js-hamburger-cta", (e) => {
+    console.log(hamburgerMenu);
+    e.preventDefault();
+    $(e.target).toggleClass("expanded");
+    hamburgerMenu.toggleClass("mobile-menu");
+  });
+})();
 
 $(".slider").slick({
   infinite: true,
